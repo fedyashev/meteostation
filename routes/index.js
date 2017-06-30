ab95fc4615eecd9f22a6bacd8a901f3b79c89cb3
Fedyashev Andrei <fedjashev@gmail.com>
2017-06-30 06:30:14 +0300

    Clear few string with comments

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,6 @@ var obj = {
 
 var saveEveryMinutes = 30;
 var lastPostDate = new Date();
-var hourDifference = 3 * 60 * 1000;
 
 function findParameterValue(parameters, parameterName) {
   for (var i = 0; i < parameters.length; i++) {
@@ -41,8 +40,6 @@ function renderChart(res, parameterName, chartDescriptionObject) {
           return;
         }
         var dataX = JSON.stringify(weatherDataArray.map(function(weatherData) {
-          //return weatherData.date.toLocaleTimeString("ru-Ru", {hour12: false});
-          // return weatherData.date.getHours() + deltaHours;
           return (new Date(weatherData.date.toLocaleString("ru-Ru", {timeZone: "Europe/Minsk"}))).getHours();
         }).reverse());
         var dataY = JSON.stringify(weatherDataArray.map(function(weatherData) {
